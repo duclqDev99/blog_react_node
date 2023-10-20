8a8870dae5a07599db28393b41a503baf98637aa
Admin <ducdiduc761311@gmail.com>
2023-10-20 15:40:41 +0700

    chỉnh sửa phần dư thừa cho file userRoutes.js

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const knex = require('../../src/databases/knex'); // Import đối tượng Knex
 const usersController = require('../controllers/usersControllers');
 const {checkPermission} = require('../middlewares/checkPermissions');
 
