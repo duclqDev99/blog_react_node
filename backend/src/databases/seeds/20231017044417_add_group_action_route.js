3d89d921b33b45ad1e27f84c3a130cd2ade3285d
Admin <ducdiduc761311@gmail.com>
2023-10-20 15:37:58 +0700

    thêm dữ liệu mẫu cho data và chỉnh sửa thêm trường cho bảng

--- backend/src/databases/seeds/20231017044417_add_group_action_route.js
+++ backend/src/databases/seeds/20231017044417_add_group_action_route.js
@@ -11,6 +11,50 @@ exports.seed = async function(knex) {
             id_route_action: 1,
           },
           {
+            id_group: 1,
+            id_route_action: 2,
+          },
+          {
+            id_group: 1,
+            id_route_action: 3,
+          },
+          {
+            id_group: 1,
+            id_route_action: 4,
+          },
+          {
+            id_group: 1,
+            id_route_action: 5,
+          },
+          {
+            id_group: 1,
+            id_route_action: 6,
+          },
+          {
+            id_group: 1,
+            id_route_action: 7,
+          },
+          {
+            id_group: 1,
+            id_route_action: 8,
+          },
+          {
+            id_group: 1,
+            id_route_action: 9,
+          },
+          {
+            id_group: 1,
+            id_route_action: 10,
+          },
+          {
+            id_group: 1,
+            id_route_action: 11,
+          },
+          {
+            id_group: 1,
+            id_route_action: 12,
+          },
+          {
             id_group: 2,
             id_route_action: 2,
           },
