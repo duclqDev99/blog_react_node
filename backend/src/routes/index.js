9bf95b055de98537202734eddd3e44f0a27a9cc3
Admin <ducdiduc761311@gmail.com>
2023-10-20 15:39:38 +0700

    thêm chức năng check token cho dự án

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const usersRoutes = require('./usersRoutes');
+const actionsRoutes = require('./actionsRoutes');
 const authenticateRoutes = require('./authenticateRoutes');
 const {verifyToken} = require('../middlewares/verifyToken');
 
-
-router.use('/users', verifyToken,usersRoutes);
 router.use('/auth',authenticateRoutes);
+router.use('/users', verifyToken,usersRoutes);
+router.use('/actions',verifyToken, actionsRoutes);
 
 module.exports = router;
